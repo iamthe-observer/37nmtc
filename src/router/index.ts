879f38452d68fff4e0adf856bc78f8ec1452fc9a
Ransford Larbi <topsquad3552@gmail.com>
2022-11-16 05:04:03 +0000

    37-NMTC init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
+import Simple from '@/views/Simple.vue'
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView,
+  // },
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Simple,
   },
   {
     path: '/about',
@@ -13,13 +19,14 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
---
+++ src/views/Simple.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="absolute inset-0 my-0 mx-auto bg-purple-400 flex justify-center items-center"
+  >
+    <LoadingScreen class="" v-if="!tick"></LoadingScreen>
+
+    <input v-else type="text" value="Hello" name="" id="" />
+
+    <ButtonLoading></ButtonLoading>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import ButtonLoading from './ButtonLoading.vue'
+import LoadingScreen from './LoadingScreen.vue'
+
+const tick = ref<boolean>(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    tick.value = true
+  }, 5000)
+})
+</script>
